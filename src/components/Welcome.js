c8918ab2bb952f381383a153a2377e773a909bf0
Timowest12 <timowester@gmail.com>
2021-12-01 12:59:29 +0100

    Add screens

---
+++ src/components/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Welcome = () => {
+    return (
+        <div class='screen'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium cupiditate quia molestiae eius modi. Ipsum sed commodi velit provident rem officiis necessitatibus cupiditate, iste temporibus? Ipsa consectetur doloremque ab architecto?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In praesentium laudantium, aspernatur accusamus blanditiis veritatis ex magnam asperiores esse fugit ipsam nam odit nemo fuga, error, repudiandae soluta enim distinctio!</p>
+        </div>
+    )
+}
---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Calculator from '../components/Calculator'
+import Header from '../components/Header'
+import { Welcome } from '../components/Welcome'
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <Welcome />
+        </div>
+    )
+}
+
+export default Home
