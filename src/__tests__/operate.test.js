51fe08056696a50c41232cfec913d628c721a972
Timowest12 <timowester@gmail.com>
2021-12-02 11:56:52 +0100

    Add correct number and operator test

--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -25,8 +25,30 @@ describe('testing operations', () => {
   });
   test('test correct number is outputted', () => {
     const numb1 = 22;
-    const numb2 = 12
-    expect(operate(numb1, numb2, 'x')).toBe('264');
+    const numb2 = 12;
+    let tobeoutputted = 22 * 12;
+    tobeoutputted = tobeoutputted.toString(10);
+    
+    expect(operate(numb1, numb2, 'x')).toBe(tobeoutputted);
   });
-
+  test('test correct - operator', () => {
+    let operator = '-'
+    let ifcorrect = '5';
+    expect(operate(10, 5, operator)).toBe(ifcorrect);
+  });
+  test('test correct + operator', () => {
+    let operator = '+'
+    let ifcorrect = '15';
+    expect(operate(10, 5, operator)).toBe(ifcorrect);
+  });
+  test('test correct ÷ operator', () => {
+    let operator = '÷'
+    let ifcorrect = '5';
+    expect(operate(10, 2, operator)).toBe(ifcorrect);
+  });
+  test('test correct x operator', () => {
+    let operator = 'x'
+    let ifcorrect = '50';
+    expect(operate(10, 5, operator)).toBe(ifcorrect);
+  });
 });
